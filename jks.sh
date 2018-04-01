9b3dd5a0fb908323f08086cbe8572c2270d260e9
root <root@ip-172-31-29-50.ap-south-1.compute.internal>
2018-04-01 04:54:24 +0000

    1pr2018

---
+++ jks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#Name=$1
+#read myname
+read -p 'Enter your Name:' myname
+echo "$myname
+read -sp 'Enter your password' password
+
+echo "$password"
+ echo $?
+############################
+1 apr test
